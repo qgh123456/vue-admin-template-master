632a2bd6da731dee9b4680ffad7ad7c0835eb172
qgh123456 <2104484868@qq.com>
2021-01-24 22:03:43 +0800

    标签页面

--- src/views/label/index.vue
+++ src/views/label/index.vue
@@ -1,13 +1,41 @@
 <template>
   <div>
-    标签
+
   </div>
 
 </template>
 
 <script>
+  import api from '@/api/label'
     export default {
-        name: "Label"
+        name: "Label",
+
+        data() {
+          return {
+            list: [], // 封装列表数据
+            page: { // 分页对象
+              current: 1,
+              size: 20,
+              total: 0
+
+            },
+            query: {}
+          }
+        },
+
+        created() {
+          this.fetchData();
+        },
+
+        methods: {
+          fetchData() {
+            api.getList(this.query,this.page.current,this.page.size).then(response => {
+                this.list = response.data.records;
+                this.page.total = response.data.total;
+            })
+          }
+        }
+
     }
 </script>
 
