58a887efec366438ff414da736349f97bda5f192
qgh123456 <2104484868@qq.com>
2021-01-24 22:03:53 +0800

    标签页面

---
+++ src/api/label.js
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+export default {
+  // 分页条件查询列表
+  getList(query,pageNo = 1,pageSize = 20){
+    return request({
+      url: `/acs/label/getList/${pageNo}/${pageSize}`,
+      method: 'post',
+      data: query // 合并为一个新对象
+    })
+  },
+
+}
